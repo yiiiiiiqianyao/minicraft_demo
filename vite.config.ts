accf52645a2c8090119a63a98b7ae1a5db3d9fcb
伊钱耀 <yiqianyao@bytedance.com>
2026-01-25 21:13:01 +0800

    feat: revert

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // 核心：设为相对路径，打包后资源引用以./开头
-  // base: './', 
+  base: './', 
   build: {
     outDir: 'dist', // 打包输出目录（默认）
   },
